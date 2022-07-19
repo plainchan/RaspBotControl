00f97443a9acd4903295e0b6f7453ac414fd7dfe
plainchan <plainwe@gmail.com>
2022-07-19 18:20:24 +0800

    ps2lib

--- HARDWARE/PS2/ps2lib.h
+++ HARDWARE/PS2/ps2lib.h
@@ -1,69 +1,106 @@
 #ifndef __PS2LIB_H__
 #define __PS2LIB_H__
-#include "delay.h"
-#include "sys.h"
-
-
-
-#define DI   PBin(12)           //PB12  输入
-
-#define DO_H PBout(13)=1        //命令位高
-#define DO_L PBout(13)=0        //命令位低
-
-#define CS_H PBout(14)=1       //CS拉高
-#define CS_L PBout(14)=0       //CS拉低
-
-#define CLK_H PBout(15)=1      //时钟拉高
-#define CLK_L PBout(15)=0      //时钟拉低
-
 
 //These are our button constants
-#define PSB_SELECT      1
-#define PSB_L3          2
-#define PSB_R3          3
-#define PSB_START       4
-#define PSB_PAD_UP      5
-#define PSB_PAD_RIGHT   6
-#define PSB_PAD_DOWN    7
-#define PSB_PAD_LEFT    8
-#define PSB_L2          9
-#define PSB_R2          10
-#define PSB_L1          11
-#define PSB_R1          12
-#define PSB_GREEN       13
-#define PSB_RED         14
-#define PSB_BLUE        15
-#define PSB_PINK        16
-#define PSB_TRIANGLE    13
-#define PSB_CIRCLE      14
-#define PSB_CROSS       15
-#define PSB_SQUARE      26
+#define PSB_SELECT      0x0001
+#define PSB_L3          0x0002
+#define PSB_R3          0x0004
+#define PSB_START       0x0008
+#define PSB_PAD_UP      0x0010
+#define PSB_PAD_RIGHT   0x0020
+#define PSB_PAD_DOWN    0x0040
+#define PSB_PAD_LEFT    0x0080
+#define PSB_L2          0x0100
+#define PSB_R2          0x0200
+#define PSB_L1          0x0400
+#define PSB_R1          0x0800
+#define PSB_GREEN       0x1000
+#define PSB_RED         0x2000
+#define PSB_BLUE        0x4000
+#define PSB_PINK        0x8000
+#define PSB_TRIANGLE    0x1000
+#define PSB_CIRCLE      0x2000
+#define PSB_CROSS       0x4000
+#define PSB_SQUARE      0x8000
 
-//#define WHAMMY_BAR		8
+//Guitar  button constants
+#define UP_STRUM		0x0010
+#define DOWN_STRUM		0x0040
+#define LEFT_STRUM		0x0080
+#define RIGHT_STRUM		0x0020
+#define STAR_POWER		0x0100
+#define GREEN_FRET		0x0200
+#define YELLOW_FRET		0x1000
+#define RED_FRET		0x2000
+#define BLUE_FRET		0x4000
+#define ORANGE_FRET		0x8000
+#define WHAMMY_BAR		8
 
 //These are stick values
-#define PSS_RX 5                //右摇杆X轴数据
+#define PSS_RX 5
 #define PSS_RY 6
 #define PSS_LX 7
 #define PSS_LY 8
 
+//These are analog buttons
+#define PSAB_PAD_RIGHT    9
+#define PSAB_PAD_UP      11
+#define PSAB_PAD_DOWN    12
+#define PSAB_PAD_LEFT    10
+#define PSAB_L2          19
+#define PSAB_R2          20
+#define PSAB_L1          17
+#define PSAB_R1          18
+#define PSAB_GREEN       13
+#define PSAB_RED         14
+#define PSAB_BLUE        15
+#define PSAB_PINK        16
+#define PSAB_TRIANGLE    13
+#define PSAB_CIRCLE      14
+#define PSAB_CROSS       15
+#define PSAB_SQUARE      16
+
+#define SET(x,y) (x|=(1<<y))
+#define CLR(x,y) (x&=(~(1<<y)))
+#define CHK(x,y) (x & (1<<y))
+#define TOG(x,y) (x^=(1<<y))
+
+#define CLK        //PB5
+#define DAT        //PA15
+#define CMD        //PB3
+#define CS         //PB4
 
 
-extern u8 Data[9];
-extern u16 MASK[16];
-extern u16 Handkey;
 
-void PS2_Init(void);
-u8 PS2_RedLight(void);//判断是否为红灯模式
-void PS2_ReadData(void);
-void PS2_Cmd(u8 CMD);		  //
-u8 PS2_DataKey(void);		  //键值读取
-u8 PS2_AnologData(u8 button); //得到一个摇杆的模拟量
-void PS2_ClearData(void);	  //清除数据缓冲区
+
+void ps2_init(void)
+{
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #endif
-
-
-
-
-
